4399b285bdea5c4a164a4c666ed954aa1264ae25
David Gabriel <gdkimati@gmail.com>
2026-05-04 12:30:26 +0100

    Update APP.py

--- APP.py
+++ APP.py
@@ -5,10 +5,10 @@ import joblib
 import os
 
 app=Flask(__name__)
-processor_cat=joblib.load('processor_cat.joblib')
-processor_reg=joblib.load('processor_reg.joblib')
-cat_model=joblib.load('XGB_model.joblib')
-reg_model=joblib.load('LinearRegresModel.joblib')
+processor_cat=joblib.load('preprocessor/processor_cat.joblib')
+processor_reg=joblib.load('preprocessor/processor_reg.joblib')
+cat_model=joblib.load('models/XGB_model.joblib')
+reg_model=joblib.load('models/LinearRegresModel.joblib')
 @app.route('/',methods=['GET','POST'])
 def myfunc():
     predicted_yield=None
